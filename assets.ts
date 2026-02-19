f668f0dd72b9010a848d2c562d2fd2379d77c8e9
MoiraCunninghamS <moira.cunningham@student.csd509j.net>
2026-02-19 09:16:55 -0800

    added power array

---
+++ assets.ts
@@ -0,0 +1,8 @@
+//array of powerups
+let powerArray = [
+    assets.image`greenShell`,
+    assets.image`speedMushroom`,
+    assets.image`redShell`,
+    assets.image`blueShell`,
+    assets.image`lightining`,
+]
