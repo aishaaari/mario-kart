d5215af86c8f8a325c2b9fd1d7ebe894281ef54e
MoiraCunninghamS <moira.cunningham@student.csd509j.net>
2026-02-23 08:35:21 -0800

    Update pxt.json, images.g.jres, images.g.ts

---
+++ images.g.ts
@@ -0,0 +1,51 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image4":
+            case "blueShell":return img`
+.....................
+.....................
+111111.........111111
+1111111.......1111111
+.1111118888888111111.
+..11188988888988111..
+...188889999988881...
+....8889888889888....
+...888988888889888...
+...889888888888988...
+...898888888888898...
+...111111111111111...
+..11111111111111111..
+....ddddddddddddd....
+....ddddddddddddd....
+.....ddddddddddd.....
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("json", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
