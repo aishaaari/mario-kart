6fb788c3fff2331c22119d1dcefbea10ce6e835f
aishaaari <aisha.ari@student.csd509j.net>
2026-03-02 08:08:12 -0800

    found something missing in functions.ts

--- functions.ts
+++ functions.ts
@@ -6,12 +6,13 @@ function hitPower(sprite: Sprite){
     return (i + 1) * 22
 }
 //shoot powerups
+/*
 function shootPowers(xpos: Number){
     for (let value of sprites.allOfKind(SpriteKind.Projectile)){
         if (value.x == xpos){
-            if ((value.image != ) && (value.image != )){
+            if ((value.image != put something here ) && (value.image != )){
                 
             }
         }
     }
-}
+} */
